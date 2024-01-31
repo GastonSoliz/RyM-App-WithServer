117f6a494f4e1ce62634c936aec0608e8e71418d
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-01-31 20:54:13 -0300

    carrousel added

--- Client/src/components/Carrousel.jsx
+++ Client/src/components/Carrousel.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { get5Char } from "../redux/actions/characterActions";
 import { useEffect, useState } from "react";
+import style from "./carrousel.module.css";
 
 export default function Carrousel() {
   const dispatch = useDispatch();
@@ -8,11 +9,67 @@ export default function Carrousel() {
     (state) => state.characterReducer.charactersCarrousel
   );
 
-  const currentImage = useState(0);
+  const [selectIndex, setSelectedIndex] = useState(0);
+  const [autoplay, setAutoplay] = useState(true);
+
+  const prev = () => {
+    const nextIndex = selectIndex > 0 ? selectIndex - 1 : characters.length - 1;
+    setSelectedIndex(nextIndex);
+    setAutoplay(false);
+  };
+
+  const next = () => {
+    const nextIndex = selectIndex < characters.length - 1 ? selectIndex + 1 : 0;
+    setSelectedIndex(nextIndex);
+    setAutoplay(false);
+  };
+
+  const handleIndicatorClick = (index) => {
+    setSelectedIndex(index);
+    setAutoplay(false);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (autoplay) {
+        // Cambiar automáticamente a la siguiente imagen si el autoplay está habilitado
+        const nextIndex =
+          selectIndex < characters.length - 1 ? selectIndex + 1 : 0;
+        setSelectedIndex(nextIndex);
+      }
+    }, 2000); // Cambiar cada 2 segundos
+
+    return () => clearInterval(interval); // Limpiar el intervalo cuando el componente se desmonta
+  }, [autoplay, characters, selectIndex]);
 
   useEffect(() => {
     dispatch(get5Char());
-  }, [dispatch]);
+  }, []);
 
-  return <div></div>;
+  //console.log("carrousel:", characters);
+  return (
+    <div className={style.carrouselContainer}>
+      <button onClick={prev}>{"<"}</button>
+      <img
+        src={characters[selectIndex]?.image}
+        alt={characters[selectIndex]?.name}
+      />
+      <button onClick={next}>{">"}</button>
+      {characters.map((_, index) => (
+        <span
+          key={index}
+          onClick={() => handleIndicatorClick(index)}
+          style={{
+            display: "inline-block",
+            width: "10px",
+            height: "10px",
+            borderRadius: "50%",
+            backgroundColor: index === selectIndex ? "blue" : "gray",
+            margin: "0 5px",
+            cursor: "pointer",
+          }}
+        ></span>
+      ))}
+    </div>
+  );
 }
