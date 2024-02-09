10c9dfa159f2376b9929cd365325b863e850592f
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-02-08 23:55:57 -0300

    fixing alert

--- Client/src/components/Alert.jsx
+++ Client/src/components/Alert.jsx
@@ -3,7 +3,7 @@ import style from "./alert.module.css";
 export default function Alert() {
   return (
     <div className={style.alertContainer}>
-      <span>Alerta!</span>
+      <span>Debes iniciar sesion para acceder a esta propiedad!</span>
     </div>
   );
 }
