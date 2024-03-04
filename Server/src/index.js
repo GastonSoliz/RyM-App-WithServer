4ad9328b9fa0e713d6512fd73178259794b4af6e
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-04 16:15:29 -0300

    fixing server port

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,7 +2,7 @@ const server = require("./app");
 const { SERVER_PORT } = process.env;
 const { conn } = require("./DB_connection");
 
-server.listen(SERVER_PORT || 3001, "0.0.0.0", () => {
+server.listen(SERVER_PORT, () => {
   conn.sync();
   console.log(`Servidor levantado en el puerto ${SERVER_PORT}`);
 });
