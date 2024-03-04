3e0e421cc2bd3b28130a2bab4dac6fb6495d7969
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-04 13:41:08 -0300

    fixing deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,7 +2,7 @@ const server = require("./app");
 const PORT = 3001;
 const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   conn.sync();
   console.log(`Servidor levantado en el puerto ${PORT}`);
 });
