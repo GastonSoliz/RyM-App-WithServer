dbd86e46ab6342fa0182fc8063937261f51b36dc
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-03-19 20:06:18 -0300

    adding loading gif

--- Client/src/components/Carrousel.jsx
+++ Client/src/components/Carrousel.jsx
@@ -3,13 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { get5Char, getCharById } from "../redux/actions/characterActions";
 import { useEffect, useState } from "react";
 import style from "./carrousel.module.css";
+import loadingGif from "../assets/loading-gif.gif";
 
 export default function Carrousel() {
   const dispatch = useDispatch();
+
   const characters = useSelector(
     (state) => state.characterReducer.charactersCarrousel
   );
-
   const [selectIndex, setSelectedIndex] = useState(0);
   const [autoplay, setAutoplay] = useState(true);
 
@@ -52,45 +53,52 @@ export default function Carrousel() {
 
   return (
     <div className={style.carrouselContainer}>
-      <div className={style.imageContainer}>
-        <button onClick={prev} className={style.buttonsCarrousel}>
-          {"<"}
-        </button>
-        <div className={style.centeredImage}>
-          <img
-            src={characters[selectIndex]?.image}
-            alt={characters[selectIndex]?.name}
-          />
-          <div className={style.indicatorsContainer}>
-            {characters.map((_, index) => (
-              <span
-                key={index}
-                onClick={() => handleIndicatorClick(index)}
-                className={`${style.indicator} ${
-                  index === selectIndex ? style.active : ""
-                }`}
-              ></span>
-            ))}
-          </div>
+      {characters.length === 0 ? (
+        <div className={style.loading}>
+          <img src={loadingGif} alt="Carga" />
+          <span>Cargando personajes...</span>
         </div>
-        <div className={style.carrouselText}>
-          <h2>Name:</h2>
-          <h2>{characters[selectIndex]?.name}</h2>
-          <button
-            onClick={() => handleSearch(characters[selectIndex]?.id)}
-            className={style.buttonText}
-          >
-            Agregar
+      ) : (
+        <div className={style.imageContainer}>
+          <button onClick={prev} className={style.buttonsCarrousel}>
+            {"<"}
           </button>
-          <Link to={`/detail/${characters[selectIndex]?.id}`}>
-            <button className={style.buttonText}>Mas info</button>
-          </Link>
-        </div>
+          <div className={style.centeredImage}>
+            <img
+              src={characters[selectIndex]?.image}
+              alt={characters[selectIndex]?.name}
+            />
+            <div className={style.indicatorsContainer}>
+              {characters.map((_, index) => (
+                <span
+                  key={index}
+                  onClick={() => handleIndicatorClick(index)}
+                  className={`${style.indicator} ${
+                    index === selectIndex ? style.active : ""
+                  }`}
+                ></span>
+              ))}
+            </div>
+          </div>
+          <div className={style.carrouselText}>
+            <h2>Name:</h2>
+            <h2>{characters[selectIndex]?.name}</h2>
+            <button
+              onClick={() => handleSearch(characters[selectIndex]?.id)}
+              className={style.buttonText}
+            >
+              Agregar
+            </button>
+            <Link to={`/detail/${characters[selectIndex]?.id}`}>
+              <button className={style.buttonText}>Mas info</button>
+            </Link>
+          </div>
 
-        <button onClick={next} className={style.buttonsCarrousel}>
-          {">"}
-        </button>
-      </div>
+          <button onClick={next} className={style.buttonsCarrousel}>
+            {">"}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
